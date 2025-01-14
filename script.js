53f4d5d5d526b5452d28e96307f9bf41cb68542f
Mateo García <mateogarcia-externo@tgacompany.com>
2025-01-14 20:48:58 -0300

    First iteration of tours grid

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const translations = {
         "explore-link": "Explorar",  
         "about-link": "Sobre Nosotros", 
         title: "URBAN TOURS",
-        subtitle: "Explora Buenos Aires, descubra lo ",
+        subtitle: "Explora Buenos Aires, descubre lo ",
         exploreHeading: "Explora Buenos Aires",
         aboutHeading: "Sobre Nosotros",
-        welcome: "Bienvenido",
-        welcome_sub: "Descubra nuestros increíbles servicios y descubra lo que podemos hacer por usted.",
-        explore_text: "Urban Tours Buenos Aires se enorgullece de ser uno de los operadores turísticos más reputados de Argentina, enfocados en organizar una amplia variedad de tours y soluciones de traslados. Nuestro grupo de especialistas en viajes experimentados y conocedores está dedicado a ofrecer un servicio de alta calidad, orientación y asistencia para hacer que tu viaje a Argentina sea emocionante y lleno de diversión. Ofrecemos una gran cantidad de oportunidades que te ayudarán a explorar los mejores destinos, principales atracciones, maravillas naturales y experimentar la rica cultura, historia y patrimonio de Buenos Aires, además de disfrutar de actividades emocionantes y aventureras. Nuestro equipo altamente experimentado puede organizar un maravilloso tour por Argentina que se adapte perfectamente a tus necesidades y presupuesto. Como especialistas en viajes en Buenos Aires, podemos organizar el mejor tour para aprovechar al máximo tu estadía. Si deseas un guía turístico confiable y experimentado en Buenos Aires, confía en nosotros. Nos dedicamos a hacer que tu viaje a Argentina sea inolvidable."
+        welcome: "¡Bienvenidos!",
+        welcome_sub: "Descubre nuestros destinos y comienza tu próxima aventura.",
+        explore_text: "Urban Tours Buenos Aires es reconocido como uno de los operadores turísticos más destacados de Argentina, especializado en ofrecer una diversa gama de recorridos y soluciones de transporte. Nuestro equipo de expertos en viajes se compromete a brindar servicios de primera calidad, asesoramiento personalizado y asistencia para que su experiencia en Argentina sea inolvidable y llena de emociones. Ponemos a tu disposición numerosas opciones para descubrir los destinos más fascinantes, las atracciones más icónicas, maravillas naturales únicas y sumergirte en la vibrante cultura, historia y legado de Buenos Aires, además de disfrutar de emocionantes actividades de aventura. Nuestro personal altamente capacitado puede organizar un recorrido excepcional por la Ciudad de Buenos Aires, diseñado para ajustarse a tus preferencias y presupuesto. Como especialistas en turismo, creamos itinerarios personalizados para maximizar tu tiempo en el país. Si buscas un guía profesional, experimentado y de confianza, somos la elección perfecta. Nos apasiona convertir tu visita a Argentina en una experiencia única."
     },
     pt: {
         about_text:"Somos uma equipe dedicada a proporcionar a melhor experiência para nossos clientes.",
@@ -28,7 +28,7 @@ const translations = {
         subtitle: "Explore Buenos Aires, descubra o ",
         exploreHeading: "Explore Buenos Aires",
         aboutHeading: "Sobre Nós",
-        welcome: "Bem-vindo",
+        welcome: "Bem-vindos!",
         welcome_sub: "Descubra nossos incríveis serviços e descubra o que podemos fazer por você.",
         explore_text:"A Tours Buenos Aires se orgulha de ser uma das mais conceituadas operadoras de turismo da Argentina, com foco na organização de uma ampla variedade de passeios e soluções de traslado. Nosso grupo de especialistas em viagens, experientes e bem informados, dedica-se a oferecer serviços, orientação e assistência de alta qualidade para tornar sua viagem à Argentina empolgante e divertida. Oferecemos uma grande variedade de oportunidades que o ajudarão a explorar os melhores destinos, as principais atrações, as maravilhas naturais e a conhecer a rica cultura, história e patrimônio de Buenos Aires, além de desfrutar de atividades emocionantes e aventureiras. Nossa equipe altamente experiente pode organizar um passeio maravilhoso pela Argentina que se adapte perfeitamente às suas necessidades e ao seu orçamento. Como especialistas em viagens a Buenos Aires, podemos organizar o melhor passeio para aproveitar ao máximo sua estadia. Se você quer um guia turístico confiável e experiente em Buenos Aires, confie em nós. Estamos empenhados em tornar sua viagem à Argentina inesquecível."
     },
@@ -42,7 +42,7 @@ const translations = {
         subtitle: "Explore Buenos Aires, discover the ",
         exploreHeading: "Explore Buenos Aires",
         aboutHeading: "About Us",
-        welcome: "Welcome",
+        welcome: "Welcome!",
         welcome_sub: "Discover our amazing services and find out what we can do for you!",
         explore_text:"Tours Buenos Aires is proud to be rated as one of the most reputable tour operators in Argentina that focuses on arranging a wide variety of tours and transfer solutions. Our group of experienced and knowledgeable travel specialists is dedicated to delivering superior-quality service, guidance, and assistance to make your Argentina trip an exciting and fun-filled affair. We offer a wealth of opportunities that help you explore the best destinations, top attractions, natural wonders, and experience the rich culture, history, and heritage of Buenos Aires as well as indulge in exciting and adventurous activities. Our highly experienced team can arrange a wonderful Argentina tour that can be suitably tailored to meet your requirements and budget. As the travel specialists in Buenos Aires, we can arrange for you the best tour to make the most of your stay. If you want a reliable and knowledgeable travel guide in Buenos Aires, then trust us. We are dedicated to making your trip to Argentina a memorable one."
     }
